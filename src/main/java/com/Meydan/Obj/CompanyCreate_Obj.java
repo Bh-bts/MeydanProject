560798b4b7dc8ee08bfaa55086c7932dd8ab12a7
bhavin thumar <thumarbhavin.786@gmail.com>
2022-12-14 21:52:57 +0530

    14/12/2022

--- src/main/java/com/Meydan/Obj/CompanyCreate_Obj.java
+++ src/main/java/com/Meydan/Obj/CompanyCreate_Obj.java
@@ -19,17 +19,30 @@ public class CompanyCreate_Obj extends AllBrowsers{
 	public @FindBy(xpath = "//input[@type='password']") WebElement pass_txt;
 	public @FindBy(xpath = "//button[@type='submit']") WebElement submit_btn;
 	
-	//Before start company
+	//Before start company creation
 	public @FindBy(xpath = "//button[@class='btn btn-block btn-secondary']") WebElement setup_cmp_btn;
 	public @FindBy(xpath = "//button[@aria-label='Close']") WebElement close_btn;
 	public @FindBy(xpath = "//h3[text()='Setup New Company ']/ancestor::div[@class='formation cursor-pointer ng-star-inserted']") WebElement setup_new_cmp_btn;
 	public @FindBy(xpath = "//span[text()='Start Process']/ancestor::button") WebElement start_process_btn;
 	
-	//Formation package
+	//Formation package (Page 1)
 	public @FindBy(xpath = "//h2[text()='Establishment package']") WebElement establishment_text;
 	public @FindBy(xpath = "//span[text()='Standard Formation']/ancestor::label/span[1]") WebElement establishment_type;
 	public @FindBy(xpath = "//strong[text()='Limited Liability Company']/ancestor::label/span") WebElement company_type;
 	public @FindBy(xpath = "//button[@type='submit']") WebElement next_btn1;
+	
+	//Business Activity (Page 2)
+	public @FindBy(xpath = "//span[text()='Help me select my business activities']/ancestor::label/span[1]") WebElement business_acitivity;
+	public @FindBy(css = ".material_label_checkbox") WebElement businessCate_chkbox1; 
+	public @FindBy(css = ".material_label_checkbox") WebElement businessSubCare_chkbox1; 
+	
+	public @FindBy(xpath = "//a[text()=' 1810.00 - Printing and service activities related to printing  ']/button") WebElement suggestAct_box1;
+	public @FindBy(css = ".material_label_checkbox") WebElement Print_chkbox1; 
+	
+	public @FindBy(xpath = "//a[text()=' 1820.00 - Reproduction of recorded media  ']/button") WebElement suggestAct_box2;
+	public @FindBy(css = ".material_label_checkbox") WebElement Reproduction_chkbox1;
+	public @FindBy(xpath = "//button[@type='submit']") WebElement next_btn2;
+	
 	
 	
 	
--- src/main/java/com/Meydan/testcase/CompanyCreate_Page.java
+++ src/main/java/com/Meydan/testcase/CompanyCreate_Page.java
@@ -14,43 +14,67 @@ import com.Meydan.Obj.CompanyCreate_Obj;
  * @author Bhavin.Thumar
  *
  */
-public class CompanyCreate_Page extends CompanyCreate_Obj{
-	
-	JavascriptExecutor js = (JavascriptExecutor) driver;
-	
+public class CompanyCreate_Page extends CompanyCreate_Obj {
+
 	public void newCompany(String id, String password) {
-		
+
 		try {
-		
-		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(60));
-		
-		email_txt.sendKeys(id);
-		pass_txt.sendKeys(password);
-		js.executeScript("arguments[0].scrollIntoView(true);", submit_btn);
-		submit_btn.click();
-		
-		w.until(ExpectedConditions.visibilityOf(setup_cmp_btn));
-		
-		setup_cmp_btn.click();
-		close_btn.click();
-		setup_new_cmp_btn.click();
-		start_process_btn.click();
-		
-		w.until(ExpectedConditions.visibilityOf(establishment_text));
-		
-		establishment_type.click();
-		w.until(ExpectedConditions.visibilityOf(company_type));
-		js.executeScript("arguments[0].scrollIntoView(true);", company_type);
-		company_type.click();
-		next_btn1.click();
-		
-		
+
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(100));
+
+			email_txt.sendKeys(id);
+			pass_txt.sendKeys(password);
+			js.executeScript("arguments[0].scrollIntoView(true);", submit_btn);
+			submit_btn.click();
+
+			w.until(ExpectedConditions.visibilityOf(setup_cmp_btn));
+
+			setup_cmp_btn.click();
+			close_btn.click();
+			setup_new_cmp_btn.click();
+			start_process_btn.click();
+
+			w.until(ExpectedConditions.visibilityOf(establishment_text));
+
+			establishment_type.click();
+			w.until(ExpectedConditions.visibilityOf(company_type));
+			js.executeScript("arguments[0].scrollIntoView(true);", company_type);
+			company_type.click();
+			
+			next_btn1.click();
+
+			w.until(ExpectedConditions.visibilityOf(business_acitivity));
+			business_acitivity.click();
+			w.until(ExpectedConditions.visibilityOf(businessCate_chkbox1));
+			js.executeScript("arguments[0].scrollIntoView(true);", businessCate_chkbox1);
+			String businessCate_chkbox = ".material_label_checkbox";
+			((JavascriptExecutor) driver).executeScript("document.querySelector(\"label[for='category_1']\",':before').click();",businessCate_chkbox);
+			
+			w.until(ExpectedConditions.visibilityOf(businessSubCare_chkbox1));
+			js.executeScript("arguments[0].scrollIntoView(true);", businessSubCare_chkbox1);
+			String businessSubCare_chkbox = ".material_label_checkbox";
+			((JavascriptExecutor) driver).executeScript("document.querySelector(\"label[for='subCategory_2']\",':before').click();",businessSubCare_chkbox);
+			
+			w.until(ExpectedConditions.visibilityOf(suggestAct_box1));
+			suggestAct_box1.click();
+			js.executeScript("arguments[0].scrollIntoView(true);", Print_chkbox1);
+			String Print_chkbox = ".material_label_checkbox";
+			((JavascriptExecutor) driver).executeScript("document.querySelector(\"label[for='group_checkbox_1']\",':before').click();",Print_chkbox);
+			
+			js.executeScript("arguments[0].scrollIntoView(true);", suggestAct_box2);
+			suggestAct_box2.click();
+			js.executeScript("arguments[0].scrollIntoView(true);", Reproduction_chkbox1);
+			String Reproduction_chkbox = ".material_label_checkbox";
+			((JavascriptExecutor) driver).executeScript("document.querySelector(\"label[for='group_checkbox_2']\",':before').click();",Reproduction_chkbox);
+			
+			next_btn2.click();
+
 		} catch (NoSuchElementException e) {
 			System.out.println(e.getMessage());
-			
+
 		}
-		
-		
+
 	}
 
 }
